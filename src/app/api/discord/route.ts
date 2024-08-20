33cb95b89a1ddbe583eea2ec5b76168daa456090
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-08-20 17:22:16 +0700

    discord alert

--- src/app/api/discord/route.ts
+++ src/app/api/discord/route.ts
@@ -1,15 +1,21 @@
 export async function POST(req: Request): Promise<Response> {
+    const body = await req.json();
+
+    const message = body.data.rows[0].name + ' has submitted a new form!';
+
     const res = await fetch(process.env.DISCORD_WEBHOOK_URL!, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            content: 'Hello, world!1',
+            content: message,
         }),
     });
-    if (!res.ok) {
-        throw new Error(`Failed to send message to Discord: ${res.statusText}`);
+
+    if (res.ok) {
+        return new Response('Success');
+    } else {
+        return new Response('Failed to send message', { status: 500 });
     }
-    return new Response('OK');
 }
