c6235e8df1d42f8ddffa664a524e64caaa7cad36
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-08-20 17:28:29 +0700

    chore: Remove commented out console.log and improve success response in discord route

--- src/app/api/discord/route.ts
+++ src/app/api/discord/route.ts
@@ -1,6 +1,7 @@
 export async function POST(req: Request): Promise<Response> {
     const body = await req.json();
-    console.log(body);
+    // console.log(body);
+    console.log(body.data.rows[0]);
     const message = body.data.rows[0].name + ' has submitted a new form!';
 
     const res = await fetch(process.env.DISCORD_WEBHOOK_URL!, {
@@ -14,7 +15,7 @@ export async function POST(req: Request): Promise<Response> {
     });
 
     if (res.ok) {
-        return new Response('Success');
+        return new Response('Success', { status: 200 });
     } else {
         return new Response('Failed to send message', { status: 500 });
     }
