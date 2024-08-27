088640da43c7d474889aaa25cb01eff473cd5129
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-08-27 15:46:22 +0700

    update route

--- src/app/api/discord/route.ts
+++ src/app/api/discord/route.ts
@@ -1,22 +1,47 @@
+const env = {
+  DISCORD_BOT_FORM_TEST_URL: process.env["WEB_APP_DISCORD_BOT_FORM_TEST_URL"],
+};
+
 export async function POST(req: Request): Promise<Response> {
-    const body = await req.json();
-    // console.log(body);
-    console.log(body.data.rows[0]);
-    const message = body.data.rows[0].Name + ' has submitted a new form!';
+  const { data } = await req.json();
+  const discordChannel = req.headers.get("Discord-Channel");
 
-    const res = await fetch(process.env.DISCORD_WEBHOOK_URL!, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            content: message,
-        }),
+  console.log("discordChannel", discordChannel);
+
+  if (!discordChannel) {
+    return new Response(
+      JSON.stringify({ error: "Discord channel not provided" }),
+      { status: 400 }
+    );
+  }
+
+  const discordChannelEnv = `DISCORD_${discordChannel
+    .toUpperCase()
+    .replace(/-/g, "_")}_URL` as keyof typeof env;
+
+  if (!env[discordChannelEnv]) {
+    return new Response(JSON.stringify({ error: "Invalid discord channel" }), {
+      status: 400,
     });
+  }
 
-    if (res.ok) {
-        return new Response('Success', { status: 200 });
-    } else {
-        return new Response('Failed to send message', { status: 500 });
-    }
+  const message = `${data.rows[0].Name} has submitted a new form!`;
+
+  const response = await fetch(env[discordChannelEnv], {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      content: message,
+    }),
+  });
+
+  if (!response.ok) {
+    return new Response(JSON.stringify({ error: "Failed to send message" }), {
+      status: response.status,
+    });
+  }
+
+  return new Response(null, { status: 204 });
 }
